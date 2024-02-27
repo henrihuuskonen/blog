c9864d0c5ccf7dfa4f5ca11e4f014463455ae0fa
Henri Huuskonen <henri@swappie.com>
2024-02-27 15:50:51 +0200

    Remove unnecessary stuff to personalize

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Henri Huuskonen';
+export const SITE_DESCRIPTION = 'Hey there! My name is Henri Huuskonen and I am a software developer. I write about programming, web development, and other tech-related topics.';
