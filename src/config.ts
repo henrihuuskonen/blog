567d0ff179ec9c6a71e1c409808cd2444d1785d2
Henri Huuskonen <henri@swappie.com>
2024-05-31 22:03:43 +0300

    Config and styling tweaks

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-export const SITE_FAVICON = "/favicon.png";
-export const SITE_LOGO = "/avatar.png";
-export const SITE_TITLE = "Mia";
+export const SITE_FAVICON = "/favicon.ico";
+export const SITE_LOGO = "/henrihuuskonen-circle.png";
+export const SITE_TITLE = "Henri Huuskonen - Blog";
 export const SITE_DESCRIPTION =
-  "Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
+  "Experienced Senior Software Engineer from Helsinki, Finland adept at optimizing business operations with React, NodeJS, Python.";
 
 export const MENUS = [
   {
@@ -22,11 +22,11 @@ export const MENUS = [
   },
   {
     title: "GitHub",
-    link: "https://github.com/infinity-ooo/astro-theme-mia",
+    link: "https://github.com/henrihuuskonen/blog",
     target: "_blank",
   },
 ];
 
-export const FOOTER_CONTENT = "Let's build a 1B unicorn 🦄";
+export const FOOTER_CONTENT = "";
 
 export const GOOGLE_GTAG = "G-XXXXXXXXXX";
